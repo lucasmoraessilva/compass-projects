3dd53d7c04813e3cbaf7ad91d8b47520ffc299c7
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-12-22 14:56:18 -0300

    feat(udemy-course): conclusão do exercício Blog Post Object proposto no curso JS Basics for Beginners

---
+++ js-basics-for-beginners/blogPostObjectExercise.js
@@ -0,0 +1,15 @@
+let post = {
+    title: 'Post Title',
+    body: 'Post body',
+    author: 'Post author',
+    views: 12,
+    comments: [
+        {
+            author: 'Comment1 author',
+            body: 'Comment1 body'
+        }
+    ],
+    isLive: true
+};
+
+console.log(post);
